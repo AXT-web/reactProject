ebb0831a4b6c4d888d44771d3a143db2eaa4c5de
AXT-web <m18775921835@163.com>
2021-05-06 09:25:51 +0800

    day3

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <Router>
       <div className="App">
         {/* 默认路由匹配时，跳转到 /home 实现路由重定向到首页 */}
-        <Route path="/" exact render={() => <Redirect to="/home" />} />
+        <Route path="/" exact render={() => <Redirect to="/home/index" />} />
         {/* 配置路由 */}
         {/* Home 组件是父路由的内容 */}
         <Route path="/home" component={Home} />
