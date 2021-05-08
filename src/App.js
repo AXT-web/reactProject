61ecb789c533eb993e0041eeaef03417fff5f81c
AXT-web <m18775921835@163.com>
2021-05-08 16:57:16 +0800

    day9

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Home from './pages/Home'
 import CityList from './pages/CityList'
 import Map from './pages/Map'
+// 房源详情组件
+import HouseDetail from './pages/HouseDetail'
 
 function App() {
   return (
@@ -25,6 +27,7 @@ function App() {
         <Route path="/home" component={Home} />
         <Route path="/citylist" component={CityList} />
         <Route path="/map" component={Map} />
+        <Route path="/detail/:id" component={HouseDetail} />
       </div>
     </Router>
   )
